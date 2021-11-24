d6af2d4acf9ab11c84ed6a83c1266040873db634
Rasmus Secher <RSeche19@student.aau.dk>
2021-11-24 16:32:12 +0100

    Filename and fileextensions - Also preferences v1

--- CleanMyData/clean/urls.py
+++ CleanMyData/clean/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import addCleaner, success
+from .views import addCleaner_view, success_view, preferences_view
 
 urlpatterns = [
-    path('', addCleaner, name='add-cleaner'),
-    path('success/', success, name='success'),
-]
+    path('', addCleaner_view, name='add-cleaner'),
+    path('success/', success_view, name='success'),
+    path('preferences/<int:pk>', preferences_view, name='preferences')
+]   
