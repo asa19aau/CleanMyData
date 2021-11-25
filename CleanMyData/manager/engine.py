88fc05c4e04c97f5fa2401e7f6b31c19c84ba565
FuSkrN <madsbisgaardsc@gmail.com>
2021-11-25 14:56:05 +0100

    found a way to extract values and column names from nested JSON objects

--- CleanMyData/manager/engine.py
+++ CleanMyData/manager/engine.py
@@ -1,16 +1,19 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as F
+
+spark = SparkSession.builder.appName('engine').getOrCreate()
 
 class engine:
     def __init__(self, filePath, fileType):
-        spark = SparkSession.builder.getOrCreate()
         if fileType == '.csv':
-            self.dataframe = spark.read.csv(filePath, header=True)
+            self.dataframe = spark.read.csv(filePath, header=True, inferSchema=True)
         elif fileType == '.json':
             self.dataframe = spark.read.option("multiline","true").json(filePath)
         elif fileType == '.tsv':
             self.dataframe = spark.read.option("delimiter", "\t").csv(filePath, header=True)
         elif fileType == '.xml':
-            self.dataframe = spark.read.format('xml').load(filePath)
+            pass
+            #todo: call function that reads xml document- one below doesnt work!!!
+            #self.dataframe = spark.read.format('xml').load(filePath)
 
     def printTable(self):
         self.dataframe.show()
@@ -18,6 +21,19 @@ class engine:
     def getColumnNames(self):
         return self.dataframe.columns
 
-test = engine('employee.xml', '.xml')
+    def flattenJSON(self):
+        pass
 
-test.printTable()
+test = engine('sample2.json', '.json')
+
+test.dataframe.show()
+
+test.dataframe.printSchema()
+
+#collapses the streetaddress attribute from the nested address attribute
+#test.dataframe.withColumn('address', F.col('address').getField('streetAddress')).show()
+
+#convert value in address that is a struct into a dict 
+address = test.dataframe.collect()[0][0].asDict()
+print(address)
+
