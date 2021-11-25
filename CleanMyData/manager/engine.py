d135d0b11c47e94fd50a27dff367a97f2f33fa4c
FuSkrN <madsbisgaardsc@gmail.com>
2021-11-25 09:59:39 +0100

    further work on reading files

--- CleanMyData/manager/engine.py
+++ CleanMyData/manager/engine.py
@@ -5,14 +5,19 @@ class engine:
         spark = SparkSession.builder.getOrCreate()
         if fileType == '.csv':
             self.dataframe = spark.read.csv(filePath, header=True)
-        elif filetype == '.json':
-            self.dataframe = spark.read.json(filePath)
-        elif filetype == '.tsv':
+        elif fileType == '.json':
+            self.dataframe = spark.read.option("multiline","true").json(filePath)
+        elif fileType == '.tsv':
             self.dataframe = spark.read.option("delimiter", "\t").csv(filePath, header=True)
-        elif filetype == '.xml':
-            self.dataframe = spark.read.xml(filePath)
+        elif fileType == '.xml':
+            self.dataframe = spark.read.format('xml').load(filePath)
+
+    def printTable(self):
+        self.dataframe.show()
 
     def getColumnNames(self):
         return self.dataframe.columns
 
-test = engine('GlobalTemperatures.csv')
+test = engine('employee.xml', '.xml')
+
+test.printTable()
