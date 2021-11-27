c69ffdcd904ce14e1c32d509ec3aba276f7caf22
StenKirk <42958111+StenKirk@users.noreply.github.com>
2021-11-27 17:20:32 +0100

    Added further additional methods

--- CleanMyData/manager/modules/temperatureModule.py
+++ CleanMyData/manager/modules/temperatureModule.py
@@ -1,17 +1,29 @@
-from re import A
-import pyspark.pandas as pan
+import pyspark.pandas as pan, standardMethods.average
+from CleanMyData.manager.modules.module import Module
 
-#Only works if temperature is in celcius
+class TemperatureModule(Module):
+#Only functions if temperature is in celcius
 
-def minimumTemperatureConstraint(dataFrame: pan.DataFrame):
-    minTemp = (-273.15)
-    df = dataFrame.where(dataFrame > minTemp)
-    return df.dropna(axis=0)
+    def minimumTemperatureConstraint(dataFrame: pan.DataFrame):
+        minTemp = (-273.15)
+        df = dataFrame.where(dataFrame > minTemp)
+        return df.dropna(axis=0)
 
-def TemperatureClimateRangeConstraint(dataFrame: pan.DataFrame, climateZoneLabel: str, temperatureLabel: str):
-    df = dataFrame.where((dataFrame[climateZoneLabel] == "Polar") and (dataFrame[temperatureLabel] > 10), dataFrame = None)
-    df = df.where((df[climateZoneLabel] == "Tropical") and (df[temperatureLabel] < 22), df = None)
-    return df.dropna(axis=0)
+    def temperatureClimateRangeConstraint(dataFrame: pan.DataFrame, climateZoneLabel: str, temperatureLabel: str):
+        df = dataFrame.where(((dataFrame[climateZoneLabel] == 'Polar') and (dataFrame[temperatureLabel] > 10) 
+        or (dataFrame[climateZoneLabel] == 'Tropical') and (dataFrame[temperatureLabel] < 22)), other=None)
+        return df.fillna(value=standardMethods.average.calculateAverage(dataFrame), axis=0)
+
+    def removeUnwantedCountries(dataFrame: pan.DataFrame, countryLabel, unwantedCountry):
+        df = dataFrame.where(dataFrame[countryLabel] == unwantedCountry, other=None)
+        return df.dropna(axis=0)
+
+    def removeTemperatureUncertainty(dataFrame: pan.DataFrame, uncertantyLabel, acceptableUncertainty: int):
+        df = dataFrame.where(dataFrame[uncertantyLabel] > acceptableUncertainty, other=None)
+        return df.dropna(axis=0)
+
+
+
 
 
 
