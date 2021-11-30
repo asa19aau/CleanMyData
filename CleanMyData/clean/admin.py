72f20e162881bb96dcce4f236fdbd65738b5261a
Sarah F <sfrand19@student.aau.dk>
2021-11-30 11:13:39 +0100

    Made the "choose data for column" page
    Did a lot of frontend stuff, we made the page for selecting what type of data is in your column. For example "Temperature" or "Distance".
    
    Co-Authored-By: Rasmus Secher <RSeche19@student.aau.dk>

--- CleanMyData/clean/admin.py
+++ CleanMyData/clean/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import File, Preferences, Header
+from .models import File, HeaderPreference, Header
 
 admin.site.register(File)
-admin.site.register(Preferences)
+admin.site.register(HeaderPreference)
 admin.site.register(Header)
